b3e567329dee7bf0f89c29f72ac604b968b938ae
Dominik Szczepanik <dominik16333@gmail.com>
2023-12-02 15:56:30 +0100

    input created

---
+++ src/main/java/pl/nullpointers/hackathonbackend/inputHandler/Input.java
@@ -0,0 +1,24 @@
+package pl.nullpointers.hackathonbackend.inputHandler;
+
+
+public class Input {
+    private String material;
+    private String isolation;
+
+    private String phaseAndCable;
+
+    private String typeOfInstalation;
+
+    private String temperature;
+
+    private String soilResistivity;
+
+    private String power;
+
+    private Double cos = 0.8;
+
+
+
+
+
+}
